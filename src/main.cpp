7a76cc1264cb50d9651bbbbefb930c676ae934a0
Cole Cunningham <ccunnin297@gmail.com>
2017-09-29 18:48:25 -0700

    Removed window from dedicated server

--- src/main.cpp
+++ src/main.cpp
@@ -29,21 +29,21 @@ int main(int argc, char* argv[]) {
         ipAddress = "127.0.0.1";
         isLocalServer = true;
     }
-
-    //This has to be done first in order to connect sf::event loop to main thread
-    sf::RenderWindow window(sf::VideoMode(GAME_BOUNDS_X, GAME_BOUNDS_Y), "cpp-asteroids");
-    window.setFramerateLimit(0);
-    window.setVerticalSyncEnabled(true);
     
-    Server server(NETWORK_PORT);
     if (isDedicatedServer) {
+        Server server(NETWORK_PORT);
         //Start server -- this will block thread until process ends
         server.start(true);
-    } else if(isLocalServer) {
-        server.start();
-    }
-
-    if (!isDedicatedServer) {
+    } else {
+        //This has to be done first in order to connect sf::event loop to main thread
+        sf::RenderWindow window(sf::VideoMode(GAME_BOUNDS_X, GAME_BOUNDS_Y), "cpp-asteroids");
+        window.setFramerateLimit(0);
+        window.setVerticalSyncEnabled(true);
+        
+        Server server(NETWORK_PORT);
+        if(isLocalServer) {
+            server.start();
+        }
 
         //Start and connect client
         Client client;
@@ -52,6 +52,8 @@ int main(int argc, char* argv[]) {
         //Start client -- this will block thread until opened window is closed
         client.run(window);
     }
+
+    
 
     // Optional:  Delete all global objects allocated by libprotobuf.
     google::protobuf::ShutdownProtobufLibrary();
